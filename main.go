2f277f614d390ad587738d1228287f2de1428a17
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-07-04 22:07:26 +0330

    [modify] main.go => add AssignVariable to main func.

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	exvariable "github.com/yousefzinsazk78/testgolangprojectfirst/pkg/ex_variable"
+)
 
 func main() {
 	fmt.Println("hello, world!")
+	//call assign variable from exvariable package
+	exvariable.AssignVariable()
 }
