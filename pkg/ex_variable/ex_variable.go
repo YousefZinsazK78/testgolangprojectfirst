1e02397c93ce65be229711c2ec07d4635d03f427
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-07-04 22:02:42 +0330

    [add] ex_variable file => how to declare and assign value to variable.

---
+++ pkg/ex_variable/ex_variable.go
@@ -0,0 +1,17 @@
+package exvariable
+
+import "fmt"
+
+func AssignVariable() {
+	//first type
+	var name string = "yousef"
+	fmt.Println(name)
+
+	//second type
+	var second_name = "sina"
+	fmt.Println(second_name)
+
+	//third type or shorthand declartion
+	third_name := "foo"
+	fmt.Println(third_name)
+}
